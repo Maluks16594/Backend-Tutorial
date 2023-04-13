12874599342847c365cd216a2499dfd0addb7c8b
Maluks16594 <malulekelangutani@gmail.com>
2023-04-13 15:43:00 +0200

    connect

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "//malulekelangutani:Langu2023.@cluster0.rndpsmn.mongodb.net/test"
+    url: "mongodb+srv://malulekelangutani:Langu2023.@cluster0.rndpsmn.mongodb.net/test"
   };
